e0c9a05a0a7d16d0fc79425f25f53ce5e5b861d4
RubenJIF <rubenjames1155@gmail.com>
2014-07-05 21:21:41 -0500

    Tercera y última frase terminada
    Todas las frases terminadas.
    Mejora del código se hará en los siguientes commits

--- fraseLCD.ino
+++ fraseLCD.ino
@@ -1,20 +1,20 @@
 /*
 Will help scroll just one line in LCD
- The other will be static.
- In this case 1st line: scrolling and 2nd line: static
- Reference: http://arduino.cc/en/Tutorial/LiquidCrystal
- The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
- */
+The other will be static.
+In this case 1st line: scrolling and 2nd line: static
+Reference: http://arduino.cc/en/Tutorial/LiquidCrystal
+The circuit:
+* LCD RS pin to digital pin 12
+* LCD Enable pin to digital pin 11
+* LCD D4 pin to digital pin 5
+* LCD D5 pin to digital pin 4
+* LCD D6 pin to digital pin 3
+* LCD D7 pin to digital pin 2
+* LCD R/W pin to ground
+* 10K resistor:
+* ends to +5V and ground
+* wiper to LCD VO pin (pin 3)
+*/
 
 // include the library
 #include <LiquidCrystal.h>
@@ -28,14 +28,15 @@ int screenHeight = 2;
 
 // the two lines
 // line1 = scrolling
-String line1 = " Not TOday le ganara a union gaymin";
-// line2 = static
+String line1 = " Si buscas resultados distintos, no hagas siempre lo mismo";
 String line2 = " Einstein";
-String linea3 = " Esta es la segunda linea largaaa";
-String linea4 = " Pronto";
+String linea3 = " Si quieres tener exito, promete todo y no cumplas nada";
+String linea4 = " Napoleon";
+String linea5 = " El saber es la parte principal de la felicidad";
+String linea6 = " Socrates";
 
 // just some reference flags
-int stringStart, stringStop, stringStart2, stringStop2, stringStart3, stringStop3  = 0;
+int stringStart, stringStop, stringStart2, stringStop2, stringStart3, stringStop3 = 0;
 int scrollCursor = screenWidth;
 int scrollCursor2 = screenWidth;
 int scrollCursor3 = screenWidth;
@@ -62,11 +63,11 @@ void loop() {
     lcd.clear();
     lcd.setCursor(scrollCursor2, 0);
     lcd.print(linea3.substring(stringStart2,stringStop2));
-//    delay(0);
+// delay(0);
     if(stringStart2 == 0 && scrollCursor2 > 0){
       scrollCursor2--;
       stringStop2++;
-    } 
+    }
     else if (stringStop2 == linea3.length() && scrollCursor2 == 0) {
       stringStart2++;
     }
@@ -79,21 +80,48 @@ void loop() {
       lcd.print(linea4);
     }
   }
+  
+  ///Tercera Frase
+  if(stringStart2 > stringStop2){
+    /*lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.print(" Tercerooo");
+    */
+    lcd.clear();
+    lcd.setCursor(scrollCursor3, 0);
+    lcd.print(linea5.substring(stringStart3,stringStop3));
+    if(stringStart3 == 0 && scrollCursor3 > 0){
+      scrollCursor3--;
+      stringStop3++;
+    }
+    else if (stringStop3 == linea5.length() && scrollCursor3 == 0) {
+      stringStart3++;
+    }
+    else {
+      stringStart3++;
+      stringStop3++;
+    }
+    if(stringStart3 <= stringStop3){
+      lcd.setCursor(0,1);
+      lcd.print(linea6);
+    }
+    
+    
+  }
+  ////
 }
 
 boolean Casa(){
     if(stringStart == 0 && scrollCursor > 0){
       scrollCursor--;
       stringStop++;
-    } 
+    }
     else if (stringStop == line1.length() && scrollCursor == 0) {
       stringStart++;
     } //Codigo
     /*else if (stringStart == stringStop){
-       
-      
-    //Codigo  
-    }*/
+//Codigo
+}*/
     else {
       stringStart++;
       stringStop++;
