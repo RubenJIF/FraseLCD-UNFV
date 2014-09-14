c4ce2e8019cede7379f51f88fd51f1b1b64a8c1d
RubenJIF <rubenjames1155@gmail.com>
2014-09-14 16:38:01 -0500

    Cambios menores
    Cambios menores

--- fraseLCD.ino
+++ fraseLCD.ino
@@ -1,47 +1,33 @@
 /*
-Will help scroll just one line in LCD
-The other will be static.
-In this case 1st line: scrolling and 2nd line: static
-Reference: http://arduino.cc/en/Tutorial/LiquidCrystal
-The circuit:
+* Disposición de pines
 * LCD RS pin to digital pin 12
 * LCD Enable pin to digital pin 11
 * LCD D4 pin to digital pin 5
 * LCD D5 pin to digital pin 4
 * LCD D6 pin to digital pin 3
 * LCD D7 pin to digital pin 2
-* LCD R/W pin to ground
-* 10K resistor:
-* ends to +5V and ground
-* wiper to LCD VO pin (pin 3)
 */
-
-// include the library
 #include <LiquidCrystal.h>
 
-// init the lcd display according to the circuit
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
-// it's a 16x2 LCD so...
 int screenWidth = 16;
 int screenHeight = 2;
 
-// the two lines
-// line1 = scrolling
+// líneas impares - scroll
+// líneas pares - no scroll
 String line1 = " Si buscas resultados distintos, no hagas siempre lo mismo";
 String line2 = " Einstein";
-String linea3 = " Si quieres tener exito, promete todo y no cumplas nada";
-String linea4 = " Napoleon";
-String linea5 = " El saber es la parte principal de la felicidad";
-String linea6 = " Socrates";
+String line3 = " Si quieres tener exito, promete todo y no cumplas nada";
+String line4 = " Napoleon";
+String line5 = " El saber es la parte principal de la felicidad";
+String line6 = " Socrates";
 
-// just some reference flags
 int stringStart, stringStop, stringStart2, stringStop2, stringStart3, stringStop3 = 0;
 int scrollCursor = screenWidth;
 int scrollCursor2 = screenWidth;
 int scrollCursor3 = screenWidth;
 
-// most of the part is pretty basic
 void setup() {
   lcd.begin(screenWidth,screenHeight);
 }
@@ -50,7 +36,17 @@ void loop() {
   lcd.setCursor(scrollCursor, 0);
   lcd.print(line1.substring(stringStart,stringStop));
   delay(100);
-  Casa();
+  if(stringStart == 0 && scrollCursor > 0){
+      scrollCursor--;
+      stringStop++;
+  }
+  else if (stringStop == line1.length() && scrollCursor == 0) {
+      stringStart++;
+  } 
+  else {
+      stringStart++;
+      stringStop++;
+  }
   if(stringStart2==0){
     lcd.clear();
   }
@@ -62,13 +58,13 @@ void loop() {
   if(stringStart >= stringStop){
     lcd.clear();
     lcd.setCursor(scrollCursor2, 0);
-    lcd.print(linea3.substring(stringStart2,stringStop2));
+    lcd.print(line3.substring(stringStart2,stringStop2));
 // delay(0);
     if(stringStart2 == 0 && scrollCursor2 > 0){
       scrollCursor2--;
       stringStop2++;
     }
-    else if (stringStop2 == linea3.length() && scrollCursor2 == 0) {
+    else if (stringStop2 == line3.length() && scrollCursor2 == 0) {
       stringStart2++;
     }
     else {
@@ -77,24 +73,20 @@ void loop() {
     }
     if(stringStart2 <= stringStop2){
       lcd.setCursor(0,1);
-      lcd.print(linea4);
+      lcd.print(line4);
     }
   }
   
   ///Tercera Frase
   if(stringStart2 > stringStop2){
-    /*lcd.clear();
-    lcd.setCursor(0, 1);
-    lcd.print(" Tercerooo");
-    */
     lcd.clear();
     lcd.setCursor(scrollCursor3, 0);
-    lcd.print(linea5.substring(stringStart3,stringStop3));
+    lcd.print(line5.substring(stringStart3,stringStop3));
     if(stringStart3 == 0 && scrollCursor3 > 0){
       scrollCursor3--;
       stringStop3++;
     }
-    else if (stringStop3 == linea5.length() && scrollCursor3 == 0) {
+    else if (stringStop3 == line5.length() && scrollCursor3 == 0) {
       stringStart3++;
     }
     else {
@@ -103,27 +95,8 @@ void loop() {
     }
     if(stringStart3 <= stringStop3){
       lcd.setCursor(0,1);
-      lcd.print(linea6);
+      lcd.print(line6);
     }
-    
-    
   }
-  ////
 }
 
-boolean Casa(){
-    if(stringStart == 0 && scrollCursor > 0){
-      scrollCursor--;
-      stringStop++;
-    }
-    else if (stringStop == line1.length() && scrollCursor == 0) {
-      stringStart++;
-    } //Codigo
-    /*else if (stringStart == stringStop){
-//Codigo
-}*/
-    else {
-      stringStart++;
-      stringStop++;
-    }
-}
